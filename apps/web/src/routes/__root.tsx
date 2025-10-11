eb80a040ead0af3ef0b97b55fa38332c3b71e1c2
akinloluwami <akinkunmioye42@gmail.com>
2025-10-11 12:45:58 +0100

    Remove unused TRPC type import from RouterAppContext interface

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -2,7 +2,6 @@ import Header from "@/components/header";
 import Loader from "@/components/loader";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import type { trpc } from "@/utils/trpc";
 import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
@@ -15,7 +14,6 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import "../index.css";
 
 export interface RouterAppContext {
-	trpc: typeof trpc;
 	queryClient: QueryClient;
 }
 
