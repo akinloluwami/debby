7439843249f9509741358cee968521e2a4a4d21d
akinloluwami <akinkunmioye42@gmail.com>
2025-10-11 12:55:42 +0100

    Add setup, database, and instance routers to appRouter

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,8 +1,14 @@
 import { publicProcedure, router } from "../index";
+import { setupRouter } from "./setup";
+import { databaseRouter } from "./database";
+import { instanceRouter } from "./instance";
 
 export const appRouter = router({
 	healthCheck: publicProcedure.query(() => {
 		return "OK";
 	}),
+	setup: setupRouter,
+	databases: databaseRouter,
+	instances: instanceRouter,
 });
 export type AppRouter = typeof appRouter;
